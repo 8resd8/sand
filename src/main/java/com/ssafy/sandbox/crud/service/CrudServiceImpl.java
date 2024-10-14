24e627a4dc1070c8222b7b3b56fc9bf57cab167e
김현우 <dkanfjgwls@naver.com>
2024-10-14 11:46:51 +0900

    fix: 구조 추가 안된것 수정

--- src/main/java/com/ssafy/sandbox/crud/service/CrudServiceImpl.java
+++ src/main/java/com/ssafy/sandbox/crud/service/CrudServiceImpl.java
@@ -1,4 +1,25 @@
 package com.ssafy.sandbox.crud.service;
 
-public class CrudServiceImpl {
+import com.ssafy.sandbox.crud.response.TodosResponse;
+
+public class CrudServiceImpl implements CrudService{
+    @Override
+    public TodosResponse readTodo() {
+        return null;
+    }
+
+    @Override
+    public TodosResponse createTodo() {
+        return null;
+    }
+
+    @Override
+    public TodosResponse UpdateTodo() {
+        return null;
+    }
+
+    @Override
+    public TodosResponse DeleteTodo() {
+        return null;
+    }
 }
