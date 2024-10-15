f7e221c5ca28daab8b66589c9748952c14e40e7b
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-15 23:53:05 +0900

    feat: service에 요청 파라미터 전달, 응답

--- src/main/java/com/ssafy/sandbox/paging/contoller/PagingController.java
+++ src/main/java/com/ssafy/sandbox/paging/contoller/PagingController.java
@@ -1,28 +1,38 @@
 package com.ssafy.sandbox.paging.contoller;
 
+import com.ssafy.sandbox.crud.dto.ResponseTodo;
+import com.ssafy.sandbox.crud.service.CrudService;
 import com.ssafy.sandbox.paging.dto.RequestPaging;
+import com.ssafy.sandbox.paging.dto.ResponsePaging;
+import com.ssafy.sandbox.paging.service.PagingService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PagingController {
+    private final CrudService crudService;
+    private final PagingService pagingService;
 
     @GetMapping("/offset")
-    public ResponseEntity<HashMap<String, Object>> offsetPaging(@ModelAttribute RequestPaging requestPaging) {
-        HashMap<String, Object> response = new HashMap<>();
+    @ResponseStatus(HttpStatus.OK)
+    public ResponsePaging offsetPaging(@ModelAttribute RequestPaging requestPaging) {
         log.info("offsetPaging: {}", requestPaging);
 
-//        List<ResponsePaging> todos = new ArrayList<>();
-        response.put("massage", "정상적으로 요청되었습니다.");
+        List<ResponseTodo> pageTotos = pagingService.pagedTotos(requestPaging.size(), requestPaging.page());
+        boolean hasNext = pagingService.hasNext(requestPaging.size(), requestPaging.page());
+        log.info("pageTodo: {}", pageTotos);
 
-        return ResponseEntity.ok(response);
+        ResponsePaging responsePaging = new ResponsePaging("정상적으로 요청되었습니다.", requestPaging.page(), requestPaging.size(), hasNext, pageTotos);
+
+        return responsePaging;
     }
 }
