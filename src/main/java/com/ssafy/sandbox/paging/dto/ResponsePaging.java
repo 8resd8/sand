44d4538968962a89630ceb2bfc7799996bf6ec1a
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-15 23:38:07 +0900

    fix: 기본 생성자로 인해 쿼리 파라미터를 입력받지 못하는 에러 record로 해결

--- src/main/java/com/ssafy/sandbox/paging/dto/ResponsePaging.java
+++ src/main/java/com/ssafy/sandbox/paging/dto/ResponsePaging.java
@@ -1,10 +1,12 @@
 package com.ssafy.sandbox.paging.dto;
 
+import com.ssafy.sandbox.crud.dto.ResponseTodo;
+
 import java.util.List;
 
 public record ResponsePaging(String message, int currentPageNumber,
                              int size, boolean hasNext,
-                             List<ResponsePaging> todos) {
+                             List<ResponseTodo> todos) {
 // 접근자와 생성자, toString, equals, hashCode
 
     public ResponsePaging {
