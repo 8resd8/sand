6be6de1f6a1b5d143762d4e2da8e47f2c112a695
김현우 <dkanfjgwls@naver.com>
2024-10-16 08:58:36 +0900

    clear: 필요없는 주석, 줄 제거 및 정리

--- src/main/java/com/ssafy/sandbox/paging/dto/RequestPaging.java
+++ src/main/java/com/ssafy/sandbox/paging/dto/RequestPaging.java
@@ -2,18 +2,3 @@ package com.ssafy.sandbox.paging.dto;
 
 public record RequestPaging(int size, int page) {
 }
-
-//@ToString
-//@Getter
-//public class RequestPaging {
-//    private int size;
-//    private int page;
-//
-//    public RequestPaging() {
-//    }
-//
-//    public RequestPaging(int size, int page) {
-//        this.size = size;
-//        this.page = page;
-//    }
-//}
--- src/main/java/com/ssafy/sandbox/paging/dto/ResponsePaging.java
+++ src/main/java/com/ssafy/sandbox/paging/dto/ResponsePaging.java
@@ -7,8 +7,4 @@ import java.util.List;
 public record ResponsePaging(String message, int currentPageNumber,
                              int size, boolean hasNext,
                              List<ResponseTodo> todos) {
-// 접근자와 생성자, toString, equals, hashCode
-
-    public ResponsePaging {
-    }
 }
