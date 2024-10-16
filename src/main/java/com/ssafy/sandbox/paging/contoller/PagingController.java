c3bfd7bb37429701d1746cddc0663e1c3afcf57d
김현우 <dkanfjgwls@naver.com>
2024-10-16 15:20:23 +0900

    fix: Cursor <-> Offset 이름 변경

--- src/main/java/com/ssafy/sandbox/paging/contoller/PagingController.java
+++ src/main/java/com/ssafy/sandbox/paging/contoller/PagingController.java
@@ -1,10 +1,11 @@
 package com.ssafy.sandbox.paging.contoller;
 
 import com.ssafy.sandbox.crud.dto.ResponseTodo;
-import com.ssafy.sandbox.crud.service.CrudService;
-import com.ssafy.sandbox.paging.dto.RequestPaging;
+import com.ssafy.sandbox.paging.dto.RequestCursor;
+import com.ssafy.sandbox.paging.dto.RequestOffset;
 import com.ssafy.sandbox.paging.dto.ResponsePaging;
-import com.ssafy.sandbox.paging.service.PagingService;
+import com.ssafy.sandbox.paging.service.OffsetService;
+import com.ssafy.sandbox.paging.service.CursorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,19 +21,37 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PagingController {
 
-    private final PagingService pagingService;
+    private final CursorService CursorService;
+    private final OffsetService offsetService;
 
-    @GetMapping("/offset")
+    @GetMapping("/paging")
     @ResponseStatus(HttpStatus.OK)
-    public ResponsePaging offsetPaging(@ModelAttribute RequestPaging requestPaging) {
-        log.info("offsetPaging: {}", requestPaging);
+    public ResponsePaging cursorPaging(@ModelAttribute RequestCursor requestCursor) {
+        log.info("cursorPaging: {}", requestCursor);
 
-        List<ResponseTodo> pageTotos = pagingService.pagedTotos(requestPaging.size(), requestPaging.page());
-        boolean hasNext = pagingService.hasNext(requestPaging.size(), requestPaging.page());
-        log.info("pageTodo: {}", pageTotos);
+        List<ResponseTodo> pageTotos = CursorService.pagedTotos(requestCursor.size(), requestCursor.cursorId());
+        boolean hasNext = CursorService.hasNext(requestCursor.size(), requestCursor.cursorId());
 
-        ResponsePaging responsePaging = new ResponsePaging("정상적으로 요청되었습니다.", requestPaging.page(), requestPaging.size(), hasNext, pageTotos);
+//        ResponsePaging responsePaging = new ResponsePaging("정상적으로 요청되었습니다.",
+//                OffsetService, requestCursor.size(), hasNext, pageTotos);
+//        log.info("cursorPaging: {}", responsePaging);
+
+        return null;
+    }
+
+    @GetMapping("/cursor")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponsePaging offsetPaging(@ModelAttribute RequestOffset requestOffset) {
+        log.info("offsetPaging: {}", requestOffset);
+
+        List<ResponseTodo> pageTotos = CursorService.pagedTotos(requestOffset.size(), requestOffset.page());
+        boolean hasNext = CursorService.hasNext(requestOffset.size(), requestOffset.page());
+
+        ResponsePaging responsePaging = new ResponsePaging("정상적으로 요청되었습니다.", requestOffset.page(), requestOffset.size(), hasNext, pageTotos);
+        log.info("responsePaging: {}", responsePaging);
 
         return responsePaging;
     }
+
+
 }
