40672c1821957d7bb06e26b2b8662ad98e2f78eb
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-17 19:29:58 +0900

    fix: 실제 요구에 맞는 응답 API 수정

--- src/main/java/com/ssafy/sandbox/paging/dto/ResponseOffset.java
+++ src/main/java/com/ssafy/sandbox/paging/dto/ResponseOffset.java
@@ -5,6 +5,7 @@ import com.ssafy.sandbox.crud.dto.ResponseTodo;
 import java.util.List;
 
 public record ResponseOffset(String message, int currentPageNumber,
-                             int size, boolean hasNext,
+                             int size, int totalPage,
+                             boolean hasNext, boolean hasPrevious,
                              List<ResponseTodo> todos) {
 }
