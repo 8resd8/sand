79c2994d0214ddade36fe66b9f6d3e35cc2de97f
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-17 19:35:09 +0900

    fix: 불필요한 로그 모두 정리

--- src/main/java/com/ssafy/sandbox/crud/repository/MysqlCrudRepository.java
+++ src/main/java/com/ssafy/sandbox/crud/repository/MysqlCrudRepository.java
@@ -5,7 +5,6 @@ import com.ssafy.sandbox.crud.dto.ResponseTodo;
 import com.ssafy.sandbox.util.ResponseTodoRowMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -22,20 +21,17 @@ public class MysqlCrudRepository implements CrudRepository {
     @Override
     public void saveTodo(RequestTodo requestTodo) {
         String sql = "insert into todos (content, completed) values (?, ?)";
-        int update = jdbcTemplate.update(sql, requestTodo.getContent(), false);
-        log.info("mySql saveTodo: {}, 결과: {}", requestTodo.getContent(), update);
+        jdbcTemplate.update(sql, requestTodo.getContent(), false);
     }
 
     @Override
     public int updateToggle(Long id) {
-        log.info("mySql updateToggle: {}", id);
         String sql = "update todos set completed = !completed where id = ?";
         return jdbcTemplate.update(sql, id);
     }
 
     @Override
     public ResponseTodo findById(Long id) {
-        log.info("Mysql, findById: {}", id);
         String sql = "select * from todos where id = ?";
         return jdbcTemplate.queryForObject(sql, new ResponseTodoRowMapper(), id);
     }
@@ -43,14 +39,11 @@ public class MysqlCrudRepository implements CrudRepository {
     @Override
     public List<ResponseTodo> findAll() {
         String sql = "select id, content, completed from todos";
-        List<ResponseTodo> query = jdbcTemplate.query(sql, new ResponseTodoRowMapper());
-        log.info("Mysql, findAll: {}", query);
-        return query;
+        return jdbcTemplate.query(sql, new ResponseTodoRowMapper());
     }
 
     @Override
     public int deleteTodo(Long id) {
-        log.info("Mysql, deleteTodo: {}", id);
         String sql = "delete from todos where id = ?";
         return jdbcTemplate.update(sql, id);
     }
@@ -58,25 +51,18 @@ public class MysqlCrudRepository implements CrudRepository {
     @Override
     public List<ResponseTodo> cursorPaging(Long cursorId, int count) {
         String sql = "select * from todos where id > ? limit ?"; // 0부터 시작
-        List<ResponseTodo> query = jdbcTemplate.query(sql, new ResponseTodoRowMapper(), cursorId, count);
-        log.info("mysql, cursorData: {}", query);
-        return query;
+        return jdbcTemplate.query(sql, new ResponseTodoRowMapper(), cursorId, count);
     }
 
     @Override
     public List<ResponseTodo> offsetPaging(int size, int offset) {
         String sql = "SELECT * FROM todos limit ? OFFSET ?";
-        List<ResponseTodo> query = jdbcTemplate.query(sql, new ResponseTodoRowMapper(), size, offset);
-        log.info("mysql, offsetData: {}", query);
-        return query;
+        return jdbcTemplate.query(sql, new ResponseTodoRowMapper(), size, offset);
     }
 
     @Override
     public int getTotalCount() {
         String sql = "select count(*) from todos";
-        int count = jdbcTemplate.queryForObject(sql, Integer.class);
-        return count;
+        return jdbcTemplate.queryForObject(sql, Integer.class);
     }
-
-
 }
