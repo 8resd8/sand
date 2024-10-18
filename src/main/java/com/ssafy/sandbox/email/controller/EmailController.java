6bbb868f482290cca09b48730292707ac0b24fcb
김현우 <dkanfjgwls@naver.com>
2024-10-18 21:59:35 +0900

    feat: 이메일 전송, 인증코드 생성 기능

--- src/main/java/com/ssafy/sandbox/email/controller/EmailController.java
+++ src/main/java/com/ssafy/sandbox/email/controller/EmailController.java
@@ -1,6 +1,9 @@
 package com.ssafy.sandbox.email.controller;
 
 import com.ssafy.sandbox.email.dto.RequestEmail;
+import com.ssafy.sandbox.email.service.EmailSendService;
+import com.ssafy.sandbox.email.service.EmailVerificationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -13,19 +16,32 @@ import java.util.HashMap;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class EmailController {
 
-    @PostMapping("/email")
-    public ResponseEntity<HashMap<String, Object>> getEmail(@Validated @RequestBody RequestEmail requestEmail, BindingResult bindingResult) {
-        log.info("입력 Email: {}", requestEmail);
-        HashMap<String, Object> response = new HashMap<>();
+    private final EmailSendService emailSendService;
+    private final EmailVerificationService emailVerificationService
 
+    @PostMapping("/email")
+    public ResponseEntity<?> sendCodeToEmail(@Validated @RequestBody RequestEmail requestEmail, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            log.info("에러발생: {}", bindingResult);
-            response.put("message", "이메일 인증에 실패했습니다.");
-            return ResponseEntity.badRequest().body(response);
+            log.error("잘못된 요청: {}", bindingResult.getAllErrors());
+            return ResponseEntity.badRequest().body("잘못된 이메일 형식입니다.");
         }
-        response.put("isOk", true);
+
+        // 인증 코드 생성 및 Redis에 저장
+        String verificationCode = emailVerificationService.generateVerificationCode();
+        emailVerificationService.storeVerificationCode(requestEmail.getEmail(), verificationCode);
+
+        // 이메일 발송
+        emailSendService.sendEmailForm(requestEmail.getEmail(), "이메일 인증 코드", "인증 코드: " + verificationCode);
+
+        // 인증코드를 비교하는 서비스 로직필요
+
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("message", "이메일이 성공적으로 전송되었습니다.");
+
+
 
         return ResponseEntity.ok(response);
     }
