c373e74bbcbae0ce0ec775f2eff50da7a6e8b823
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-19 13:14:01 +0900

    refactor: 이메일 제목 상수로 정의, html 본문 내용 수정

--- src/main/java/com/ssafy/sandbox/email/service/EmailSendService.java
+++ src/main/java/com/ssafy/sandbox/email/service/EmailSendService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class EmailSendService {
 
+    public static final String EMAIL_TITLE = "Sandbox: 이메일 인증 코드";
     private final JavaMailSender javaMailSender;
 
     /// 발신할 이메일 설정
@@ -21,7 +22,7 @@ public class EmailSendService {
             MimeMessage message = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             helper.setTo(toEmail); // 누구에게 보낼건지
-            helper.setSubject("이메일 인증 코드"); // 이메일 제목
+            helper.setSubject(EMAIL_TITLE); // 이메일 제목
             helper.setText(htmlContent(authCode), true); // 이메일 본문, true 설정시 html 처리된다
 
             javaMailSender.send(message);
@@ -45,7 +46,7 @@ public class EmailSendService {
                     <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #ffffff; border: 1px solid #dddddd; border-radius: 10px;">
                         <h2 style="color: #2c3e50;">Sandbox에서 보낸 이메일입니다</h2>
                         <p style="color: #34495e; line-height: 1.6;">안녕하세요, SSAFY 12기 15반입니다</p>
-                        <p style="color: #34495e; line-height: 1.6;">요청하신 이메일 인증 코드는 아래와 같습니다:</p>
+                        <p style="color: #34495e; line-height: 1.6;">요청하신 이메일 인증 코드는 아래와 같습니다</p>
                         <br>
                         <p style="font-size: 24px; font-weight: bold; color: #e74c3c;">인증 코드: <strong>%s</strong></p>
                         <br>
@@ -53,7 +54,7 @@ public class EmailSendService {
                         <p style="color: #34495e; line-height: 1.6;">만약 본인이 요청하지 않은 이메일이라면, 이 이메일을 무시하셔도 됩니다.</p>
                         <br>
                         <p style="color: #34495e; line-height: 1.6;">문의사항이 있으면 아래로 연락주세요:</p>
-                        <p style="color: #34495e; line-height: 1.6;">SSAFY 12기 15반</p>
+                        <p style="color: #34495e; line-height: 1.6;">SSAFY 12기 15반 김현우</p>
                         <p style="color: #34495e; line-height: 1.6;">위치: SSAFY 서울캠퍼스</p>
                         <p style="color: #34495e; line-height: 1.6;">주소: 서울특별시 강남구 테헤란로 212 멀티캠퍼스</p>
                         <br><br>
