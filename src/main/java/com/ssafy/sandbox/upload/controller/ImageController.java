e446b70d55b6e7a68fa52b01c2af2a51f0b74bb6
김현우 <dkanfjgwls@naver.com>
2024-10-22 17:43:56 +0900

    feat: 파일 업로드 기본 세팅

--- src/main/java/com/ssafy/sandbox/upload/controller/ImageController.java
+++ src/main/java/com/ssafy/sandbox/upload/controller/ImageController.java
@@ -2,17 +2,44 @@ package com.ssafy.sandbox.upload.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
 
-    @PostMapping("/image")
-    public String imageUpload() {
+    @Value("${file.dir}")
+    private String fileDir;
 
+    @PostMapping("/image")
+    public String imageUpload(@RequestParam("image") MultipartFile[] multipartFiles) throws IOException {
+        // 로그로 파일 배열 크기 확인
+        log.info("Number of files: {}", multipartFiles.length);
+
+        // 파일이 존재하는지 확인
+        if (multipartFiles.length == 0 || multipartFiles[0].isEmpty()) {
+            return "파일이 없습니다.";
+        }
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            // 개별 파일 처리
+            String fullPath = fileDir + multipartFile.getOriginalFilename();
+            log.info("Full path: {}", fullPath);
+
+            // 파일 저장
+            File file = new File(fullPath);
+            multipartFile.transferTo(file);
+        }
         return "ok";
     }
 }
