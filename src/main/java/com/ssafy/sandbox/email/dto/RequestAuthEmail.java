66816bd646dccfafa9ebcfc675a81152bfdbcff6
김현우 <dkanfjgwls@naver.com>
2024-10-23 17:33:28 +0900

    fix: 프론트 API 요청 응답 버그 수정으로 인한 코드 수정

--- src/main/java/com/ssafy/sandbox/email/dto/RequestAuthEmail.java
+++ src/main/java/com/ssafy/sandbox/email/dto/RequestAuthEmail.java
@@ -6,15 +6,17 @@ import lombok.ToString;
 
 @Getter
 @ToString
-public class RequestAuthCode {
+public class RequestAuthEmail {
 
     @NotBlank
     private String authentication;
+    private String email;
 
-    public RequestAuthCode() {
+    public RequestAuthEmail() {
     }
 
-    public RequestAuthCode(String authentication) {
+    public RequestAuthEmail(String authentication, String email) {
         this.authentication = authentication;
+        this.email = email;
     }
 }
