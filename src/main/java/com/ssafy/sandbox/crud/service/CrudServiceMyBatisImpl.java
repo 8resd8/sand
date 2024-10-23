092eb1495cce4ffe9dead4e124cc03c7c0eb6aa9
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-24 00:14:08 +0900

    feat: myBatis 인터페이스를 사용하는 서비스 구현

---
+++ src/main/java/com/ssafy/sandbox/crud/service/CrudServiceMyBatisImpl.java
@@ -0,0 +1,56 @@
+package com.ssafy.sandbox.crud.service;
+
+import com.ssafy.sandbox.crud.dto.RequestTodo;
+import com.ssafy.sandbox.crud.dto.Todo;
+import com.ssafy.sandbox.crud.repository.CrudMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service(value = "myBatis")
+@RequiredArgsConstructor
+public class CrudServiceMyBatisImpl implements CrudService {
+
+    private final CrudMapper crudMapper;
+
+    @Override
+    public void saveTodo(RequestTodo requestTodo) {
+        crudMapper.saveTodo(requestTodo);
+    }
+
+    @Override
+    public int updateToggle(Long id) {
+        return crudMapper.updateToggle(id);
+    }
+
+    @Override
+    public Todo findById(Long id) {
+        return crudMapper.findById(id);
+    }
+
+    @Override
+    public List<Todo> findAll() {
+        return crudMapper.findAll();
+    }
+
+    @Override
+    public int deleteTodo(Long id) {
+        return crudMapper.deleteTodo(id);
+    }
+
+    @Override
+    public List<Todo> cursorPaging(Long cursorId, int count) {
+        return crudMapper.cursorPaging(cursorId, count);
+    }
+
+    @Override
+    public List<Todo> offsetPaging(int size, int offset) {
+        return crudMapper.offsetPaging(size, offset);
+    }
+
+    @Override
+    public int getTotalCount() {
+        return crudMapper.getTotalCount();
+    }
+}
