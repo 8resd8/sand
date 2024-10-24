027e0a89c13e7f9dd77198a9d2c160af0a7b6581
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-24 22:45:52 +0900

    save: 현재 파일 저장

--- src/main/java/com/ssafy/sandbox/social/controller/KakaoController.java
+++ src/main/java/com/ssafy/sandbox/social/controller/KakaoController.java
@@ -6,35 +6,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @RestController
+@RequestMapping("/oauth")
 public class KakaoController {
 
-    //    @PostMapping("/oauth/auth")
-    public ResponseEntity<?> auth(@RequestParam("code") String code) {
-        log.info("post auth: {}", code);
-
-        return ResponseEntity.ok(code);
+    @PostMapping("/auth")
+    public ResponseEntity<?> auth2(@RequestBody RequestTest requestTest) {
+        log.info("post auth: {}", requestTest);
+        HashMap<String, Object> response = new HashMap<>();
+//        response.put("accessToken", )
+        return ResponseEntity.ok(requestTest);
     }
 
-    @PostMapping("/oauth/auth")
-    public ResponseEntity<?> auth2(@ModelAttribute RequestTest code) {
-        log.info("post auth: {}", code);
-
-        return ResponseEntity.ok(code);
-    }
-
-    @GetMapping("/oauth/auth")
+    @GetMapping("/cookie/auth")
     public ResponseEntity<?> authq(@RequestBody Map<String, Object> code) {
         log.info("get auth: {}", code);
         return ResponseEntity.ok(code);
     }
 
-    @GetMapping("/callback")
-    public ResponseEntity<?> authorize(@RequestParam("code") String code) {
-        log.info("authorize: {}", code);
-        return ResponseEntity.ok(code);
-    }
+
 }
--- src/main/java/com/ssafy/sandbox/social/dto/RequestTest.java
+++ src/main/java/com/ssafy/sandbox/social/dto/RequestTest.java
@@ -3,9 +3,9 @@ package com.ssafy.sandbox.social.dto;
 import lombok.Data;
 import lombok.Getter;
 
-@Data
-public class RequestTest {
-    private String response_type;
-    private String  client_id;
-    private String  redirect_uri;
+public record RequestTest (String code){
+
+//    private String response_type;
+//    private String  client_id;
+//    private String  redirect_uri;
 }
