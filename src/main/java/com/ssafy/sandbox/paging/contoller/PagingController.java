acbd1e6211626edfaaeec36e6941ef15ce203bed
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-24 23:08:23 +0900

    refactor: myBatis 도입, 컨트롤러 변경

--- src/main/java/com/ssafy/sandbox/paging/contoller/PagingController.java
+++ src/main/java/com/ssafy/sandbox/paging/contoller/PagingController.java
@@ -1,8 +1,7 @@
 package com.ssafy.sandbox.paging.contoller;
 
 import com.ssafy.sandbox.paging.dto.*;
-import com.ssafy.sandbox.paging.service.CursorService;
-import com.ssafy.sandbox.paging.service.OffsetService;
+import com.ssafy.sandbox.paging.service.TotalPagingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,18 +15,17 @@ import java.util.List;
 @RequestMapping("/articles/paging")
 public class PagingController {
 
-    private final CursorService cursorService;
-    private final OffsetService offsetService;
+    private final TotalPagingService totalPagingService;
 
-    // cursor: 스크롤 내리면 알아서 다음페이지 요청 들어온다.
+
     @GetMapping("/cursor")
     @ResponseStatus(HttpStatus.OK)
     public ResponseCursor cursorPaging(@ModelAttribute RequestCursor requestCursor) {
         log.info("Cursor Input Data: {}", requestCursor);
 
-        List<Paging> pages = cursorService.pagedTotos(requestCursor.size(), requestCursor.cursorId());
-        Long lastId = cursorService.getNextCursor(pages);
-        boolean hasNext = cursorService.hasNext(requestCursor.cursorId(), requestCursor.size());
+        List<Paging> pages = totalPagingService.pagedTotos(requestCursor.size(), requestCursor.cursorId());
+        Long lastId = totalPagingService.getNextCursor(pages);
+        boolean hasNext = totalPagingService.hasNext(requestCursor.cursorId(), requestCursor.size());
 
         ResponseCursor responseCursor = new ResponseCursor(lastId, requestCursor.size(), hasNext, pages);
         log.info("Cursor Response Data: {}", responseCursor);
@@ -43,11 +41,11 @@ public class PagingController {
         int page = requestOffset.page();
         int size = requestOffset.size();
 
-        int currentPageNumber = offsetService.currentPageNumber(page);
-        int totalPage = offsetService.totalPage(size);
-        boolean hasNext = offsetService.hasNext(size, page);
-        boolean hasPrevious = offsetService.hasPrevious(page);
-        List<Paging> todos = offsetService.pagedTodos(size, page);
+        int currentPageNumber = totalPagingService.currentPageNumber(page);
+        int totalPage = totalPagingService.totalPage(size);
+        boolean hasNext = totalPagingService.hasNext(size, page);
+        boolean hasPrevious = totalPagingService.hasPrevious(page);
+        List<Paging> todos = totalPagingService.pagedTodos(size, page);
         ResponseOffset responseDate = new ResponseOffset(currentPageNumber,
                 size, totalPage, hasNext, hasPrevious, todos);
 
