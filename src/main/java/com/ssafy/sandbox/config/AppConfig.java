7201280f0ffa1904cad4fe235387127673d60297
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-28 20:21:16 +0900

    refactor : 패키지 합침

--- src/main/java/com/ssafy/sandbox/config/AppConfig.java
+++ src/main/java/com/ssafy/sandbox/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.sandbox;
+package com.ssafy.sandbox.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/ssafy/sandbox/config/WebConfig.java
+++ src/main/java/com/ssafy/sandbox/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.sandbox;
+package com.ssafy.sandbox.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
