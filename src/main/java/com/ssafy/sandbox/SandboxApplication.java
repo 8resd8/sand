e3b04ca64d65243299fa77a3ad83b2e9be530c55
RESD\dkanf <dkanfjgwls@naver.com>
2024-10-30 22:57:10 +0900

    refactor : 안쓰는 항목 수정

--- src/main/java/com/ssafy/sandbox/SandboxApplication.java
+++ src/main/java/com/ssafy/sandbox/SandboxApplication.java
@@ -3,17 +3,14 @@ package com.ssafy.sandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
-import com.ssafy.sandbox.config.JpaConfig;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-@Profile(value = "local")
 public class SandboxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SandboxApplication.class, args);
 	}
+
 
 }
