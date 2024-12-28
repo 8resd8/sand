9874d66202bc74c5184fa1cd4bff7319b2ba7e22
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-28 13:33:59 +0900

    feat: JSON 파싱을 위한 기본생성자 추가

--- src/main/java/com/ssafy/sandbox/crud/dto/TodoRequest.java
+++ src/main/java/com/ssafy/sandbox/crud/dto/TodoRequest.java
@@ -3,8 +3,10 @@ package com.ssafy.sandbox.crud.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class TodoRequest {
 
