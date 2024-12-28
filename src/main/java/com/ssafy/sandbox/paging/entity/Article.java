f1cd639e454487fc18deae7db256e05029a3dea9
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-28 17:52:28 +0900

    feat: 제약조건, auditing 추가

--- src/main/java/com/ssafy/sandbox/paging/entity/Article.java
+++ src/main/java/com/ssafy/sandbox/paging/entity/Article.java
@@ -1,26 +1,25 @@
 package com.ssafy.sandbox.paging.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
 
     @Id @GeneratedValue
     @Column(name = "article_id")
     private Long id;
 
-    @Column(name = "title")
+    @Column(name = "title", nullable = false)
     private String title;
 
     @CreatedDate
