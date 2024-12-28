00ad1f1ca18fefd129c03a736b2ed2ecbe4dba1f
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-28 15:50:47 +0900

    feat: Cursor Paging은 직접 쿼리 작성

---
+++ src/main/java/com/ssafy/sandbox/paging/repository/PageQueryRepository.java
@@ -0,0 +1,40 @@
+package com.ssafy.sandbox.paging.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.sandbox.paging.dto.PageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.ssafy.sandbox.paging.entity.QArticle.article;
+
+
+@Repository
+@RequiredArgsConstructor
+public class PageQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+
+    public List<PageDto> findByCursor(Long cursorId, int size) {
+        return queryFactory
+                .select(Projections.constructor(PageDto.class,
+                        article.id,
+                        article.title,
+                        article.createdAt))
+                .from(article)
+                .where(cursorCondition(cursorId))
+                .orderBy(article.id.asc())
+                .limit(size)
+                .fetch();
+    }
+
+    private BooleanExpression cursorCondition(Long cursorId) {
+        return article.id.gt(cursorId);
+    }
+
+
+}
