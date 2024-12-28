3745d3f7597957d5480741364919c5b19fcc089a
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-28 13:32:50 +0900

    feat: 상태코드 변경

--- src/main/java/com/ssafy/sandbox/crud/controller/CrudController.java
+++ src/main/java/com/ssafy/sandbox/crud/controller/CrudController.java
@@ -1,19 +1,14 @@
 package com.ssafy.sandbox.crud.controller;
 
 import com.ssafy.sandbox.crud.dto.TodoRequest;
-import com.ssafy.sandbox.crud.dto.TodoResponse;
+import com.ssafy.sandbox.crud.dto.TodosResponse;
 import com.ssafy.sandbox.crud.service.TodoService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@Slf4j
 @RequestMapping({"/", "/todos"})
 @RequiredArgsConstructor
 public class CrudController {
@@ -22,8 +17,8 @@ public class CrudController {
 
 
     @GetMapping
-    public ResponseEntity<List<TodoResponse>> getAll() {
-        return ResponseEntity.ok(todoService.findAllTodo());
+    public TodosResponse getAll() {
+        return todoService.findAllTodo();
     }
 
     @PostMapping
@@ -33,13 +28,13 @@ public class CrudController {
     }
 
     @PatchMapping("/{todoId}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void UpdateTodo(@PathVariable Long todoId) {
         todoService.updateToggle(todoId);
     }
 
     @DeleteMapping("/{todoId}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void DeleteTodo(@PathVariable Long todoId) {
         todoService.deleteTodo(todoId);
     }
