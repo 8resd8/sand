61a5a9730be7cace05e1a9188d39e1f86076339f
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-28 12:40:46 +0900

    feat: jpa 사용한 서비스 클래스 사용

--- src/main/java/com/ssafy/sandbox/crud/controller/CrudController.java
+++ src/main/java/com/ssafy/sandbox/crud/controller/CrudController.java
@@ -1,71 +1,46 @@
 package com.ssafy.sandbox.crud.controller;
 
-import com.ssafy.sandbox.crud.dto.RequestTodo;
-import com.ssafy.sandbox.crud.dto.ResponseTodo;
-import com.ssafy.sandbox.crud.service.CrudService;
+import com.ssafy.sandbox.crud.dto.TodoRequest;
+import com.ssafy.sandbox.crud.dto.TodoResponse;
+import com.ssafy.sandbox.crud.service.TodoService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping({"/", "/todos"})
+@RequiredArgsConstructor
 public class CrudController {
 
-    private final CrudService crudService;
+    private final TodoService todoService;
 
-    public CrudController(@Qualifier("myBatis") CrudService crudService) {
-        this.crudService = crudService;
-    }
 
     @GetMapping
-    public ResponseEntity<ResponseTodo> readTodo() {
-        String massage = "정상적으로 요청되었습니다.";
-
-        // 에러가 날 경우 메시지 변경
-        ResponseTodo response = new ResponseTodo(massage, crudService.findAll());
-
-        return ResponseEntity.ok(response);
+    public ResponseEntity<List<TodoResponse>> getAll() {
+        return ResponseEntity.ok(todoService.findAllTodo());
     }
 
     @PostMapping
-    public ResponseEntity<HashMap<String, Object>> createTodo(@Validated @RequestBody RequestTodo todos, BindingResult bindingResult) {
-        HashMap<String, Object> response = new HashMap<>();
-
-        if (bindingResult.hasErrors()) {
-            log.error("값의 문제가 있음: {}", todos);
-            response.put("message", "정상적이지 않은 요청입니다.");
-            return ResponseEntity.badRequest().body(response);
-        }
-
-        response.put("message", todos.getContent());
-        crudService.saveTodo(todos);
-
-        return ResponseEntity.ok(response);
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveTodo(@Validated @RequestBody TodoRequest request) {
+        todoService.save(request);
     }
 
     @PatchMapping("/{todoId}")
-    public ResponseEntity<Map<String, Object>> UpdateTodo(@PathVariable Long todoId) {
-        crudService.updateToggle(todoId);
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", todoId + "의 completed가 정상적으로 토글되었습니다");
-
-        return ResponseEntity.ok(response);
+    @ResponseStatus(HttpStatus.OK)
+    public void UpdateTodo(@PathVariable Long todoId) {
+        todoService.updateToggle(todoId);
     }
 
     @DeleteMapping("/{todoId}")
-    public ResponseEntity<Map<String, Object>> DeleteTodo(@PathVariable Long todoId) {
-        crudService.deleteTodo(todoId);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", todoId + "의 todo가 삭제되었습니다");
-
-        return ResponseEntity.ok(response);
+    @ResponseStatus(HttpStatus.OK)
+    public void DeleteTodo(@PathVariable Long todoId) {
+        todoService.deleteTodo(todoId);
     }
 }
