767555a0a47b42bec439888ecfff7c151f6c1ae4
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-28 21:07:15 +0900

    feat: lastId 결과 메서드 추가

--- src/main/java/com/ssafy/sandbox/paging/repository/ArticleQueryRepository.java
+++ src/main/java/com/ssafy/sandbox/paging/repository/ArticleQueryRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ArticleQueryRepository {
     List<PageDto> findByCursor(Long cursorId, int size);
+
+    long getLastId(Long cursorId, List<PageDto> result);
 }
