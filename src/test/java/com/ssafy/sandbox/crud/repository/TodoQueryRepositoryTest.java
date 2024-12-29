a8648220004a13a9671a51ee9af88fb8de292990
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-29 12:07:22 +0900

    test: 프로필 제거

--- src/test/java/com/ssafy/sandbox/crud/repository/TodoQueryRepositoryTest.java
+++ src/test/java/com/ssafy/sandbox/crud/repository/TodoQueryRepositoryTest.java
@@ -6,16 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-@ActiveProfiles("test")
 class TodoQueryRepositoryTest {
 
 
