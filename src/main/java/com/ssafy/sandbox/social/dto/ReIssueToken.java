8332c9387c81fcd29bacf9ed833fc23619f59130
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-30 17:58:34 +0900

    feat: 토큰 재발급 객체

---
+++ src/main/java/com/ssafy/sandbox/social/dto/ReIssueToken.java
@@ -0,0 +1,10 @@
+package com.ssafy.sandbox.social.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReIssueToken {
+    private String accessToken;
+}
