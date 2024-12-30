c07ff9c8fd2d3c5bca98fc8ebcc6064c822344ed
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-30 20:00:32 +0900

    fix: @Controller 쓰고있던 문제 해결

--- src/main/java/com/ssafy/sandbox/social/controller/UserController.java
+++ src/main/java/com/ssafy/sandbox/social/controller/UserController.java
@@ -10,11 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/oauth/authorization")
 public class UserController {
