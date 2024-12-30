fb50dd2fd6e4a27211a5150cb5ae94e1bb4210eb
RESD\dkanf <dkanfjgwls@naver.com>
2024-12-30 17:53:00 +0900

    feat: 닉네임 응답 dto

---
+++ src/main/java/com/ssafy/sandbox/social/dto/VerifyTokenResponse.java
@@ -0,0 +1,10 @@
+package com.ssafy.sandbox.social.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VerifyTokenResponse {
+    private String nickname;
+}
